af0dcaf0dc9679a54203f8c95e296b6a31c4b9cb
DESKTOP-Q9RO308\david <davidandrews7@hotmail.com>
2026-01-20 11:01:55 -0500

    Add imported packages script

---
+++ R/zzz_imports.R
@@ -0,0 +1,7 @@
+#' @importFrom magrittr %>%
+#' @importFrom rlang :=
+#' @importFrom fs dir_exists
+#' @importFrom checkmate assert_character
+#' @importFrom readxl read_excel
+#' @importFrom writexl write_xlsx
+NULL
